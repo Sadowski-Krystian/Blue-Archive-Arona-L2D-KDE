9c035873b60f430f04f380cd0bcba67b5ddd3667
Krystian Sadowski <krystek.sadowski@gmail.com>
2026-09-08 14:36:54 +0200

    fix: build script and install
    Signed-off-by: Krystian Sadowski <krystek.sadowski@gmail.com>

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 REPO="Sadowski-Krystian/Blue-Archive-Arona-L2D-KDE"
 echo "Fetching latest release of Blue Archive Live2D Wallpaper..."
 
-DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/${REPO}/releases/tag/latest" \
+DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/${REPO}/releases/latest" \
   | grep "browser_download_url.*plasmoid" \
   | cut -d '"' -f 4)
 
